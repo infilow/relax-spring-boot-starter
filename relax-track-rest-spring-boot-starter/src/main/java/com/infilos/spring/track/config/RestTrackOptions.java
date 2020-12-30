e98d0484daf55d6ec6a1adee85a6f62f118475cd
infilos <infilos00@gmail.com>
2020-12-30 13:41:53 +0800

    Reformat cod.

--- relax-track-rest-spring-boot-starter/src/main/java/com/infilos/spring/track/config/RestTrackOptions.java
+++ relax-track-rest-spring-boot-starter/src/main/java/com/infilos/spring/track/config/RestTrackOptions.java
@@ -6,22 +6,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties("track.record.rest")
 public class RestTrackOptions {
-  private String ignorePatterns = null;
-  private Boolean includeHeaders = false;
+    private String ignorePatterns = null;
+    private Boolean includeHeaders = false;
 
-  public String getIgnorePatterns() {
-    return ignorePatterns;
-  }
+    public String getIgnorePatterns() {
+        return ignorePatterns;
+    }
 
-  public void setIgnorePatterns(String ignorePatterns) {
-    this.ignorePatterns = ignorePatterns;
-  }
+    public void setIgnorePatterns(String ignorePatterns) {
+        this.ignorePatterns = ignorePatterns;
+    }
 
-  public boolean isIncludeHeaders() {
-    return includeHeaders;
-  }
+    public boolean isIncludeHeaders() {
+        return includeHeaders;
+    }
 
-  public void setIncludeHeaders(boolean includeHeaders) {
-    this.includeHeaders = includeHeaders;
-  }
+    public void setIncludeHeaders(boolean includeHeaders) {
+        this.includeHeaders = includeHeaders;
+    }
 }
--- relax-track-rest-spring-boot-starter/src/main/java/com/infilos/spring/track/config/RestTrackRecodFilter.java
+++ relax-track-rest-spring-boot-starter/src/main/java/com/infilos/spring/track/config/RestTrackRecodFilter.java
@@ -5,7 +5,6 @@ import com.infilos.spring.track.utils.UuidGenerator;
 import com.infilos.spring.track.wrapper.SpringRequestWrapper;
 import com.infilos.spring.track.wrapper.SpringRespondWrapper;
 import com.infilos.utils.Loggable;
-import org.apache.commons.io.IOUtils;
 import org.slf4j.MDC;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -21,111 +20,115 @@ import java.io.IOException;
 
 public class RestTrackRecodFilter extends OncePerRequestFilter implements Loggable {
 
-  private final ApplicationContext context;
-  private final UuidGenerator generator;
-  private final String ignorePatterns;
-  private final Boolean includeHeaders;
+    private final ApplicationContext context;
+    private final UuidGenerator generator;
+    private final String ignorePatterns;
+    private final Boolean includeHeaders;
 
-  public RestTrackRecodFilter(
-      ApplicationContext context, RestTrackOptions options, UuidGenerator generator) {
-    this.context = context;
-    this.generator = generator;
-    this.ignorePatterns = options.getIgnorePatterns();
-    this.includeHeaders = options.isIncludeHeaders();
-  }
-
-  @Override
-  protected void doFilterInternal(
-      HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-      throws ServletException, IOException {
-    if (ignorePatterns != null && request.getRequestURI().matches(ignorePatterns)) {
-      chain.doFilter(request, response);
-      return;
+    public RestTrackRecodFilter(
+        ApplicationContext context, RestTrackOptions options, UuidGenerator generator) {
+        this.context = context;
+        this.generator = generator;
+        this.ignorePatterns = options.getIgnorePatterns();
+        this.includeHeaders = options.isIncludeHeaders();
     }
 
-    generator.fillMdc(request);
-    fillHandlerMethod(request);
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+        throws ServletException, IOException {
+        if (ignorePatterns != null && request.getRequestURI().matches(ignorePatterns)) {
+            chain.doFilter(request, response);
+            return;
+        }
 
-    final long start = System.currentTimeMillis();
-    final SpringRequestWrapper wrappedRequest = new SpringRequestWrapper(request);
-    final SpringRespondWrapper wrappedRespond = new SpringRespondWrapper(response);
-    wrappedRespond.setHeader(Consts.ReqidHeader, MDC.get(Consts.ReqidHeader));
-    wrappedRespond.setHeader(Consts.CoridHeader, MDC.get(Consts.CoridHeader));
+        generator.fillMdc(request);
+        fillHandlerMethod(request);
 
-    try {
-      loggingRequest(wrappedRequest);
-      chain.doFilter(wrappedRequest, wrappedRespond);
-      loggingResponse(start, wrappedRespond);
-    } catch (Exception ex) {
-      loggingResponse(start, wrappedRespond, 500);
-      throw ex;
+        final long start = System.currentTimeMillis();
+        final SpringRequestWrapper wrappedRequest = SpringRequestWrapper.create(request);
+        final SpringRespondWrapper wrappedRespond = SpringRespondWrapper.create(response);
+        wrappedRespond.setHeader(Consts.ReqidHeader, MDC.get(Consts.ReqidHeader));
+        wrappedRespond.setHeader(Consts.CoridHeader, MDC.get(Consts.CoridHeader));
+
+        try {
+            loggingRequest(wrappedRequest);
+            chain.doFilter(wrappedRequest, wrappedRespond);
+            loggingResponse(start, wrappedRespond);
+        } catch (Exception ex) {
+            loggingResponse(start, wrappedRespond, 500);
+            throw ex;
+        }
     }
-  }
 
-  private void loggingRequest(SpringRequestWrapper request) {
-    try {
-      if (includeHeaders) {
-        log()
-            .info(
-                "-->Request: method={}, uri={}, body={}, headers={}",
-                request.getMethod(),
-                request.getRequestURI(),
-                IOUtils.toString(request.getInputStream(), request.getCharacterEncoding()),
-                request.getAllHeaders());
-      } else {
-        log()
-            .info(
-                "-->Request: method={}, uri={}, body={}",
-                request.getMethod(),
-                request.getRequestURI(),
-                IOUtils.toString(request.getInputStream(), request.getCharacterEncoding()));
-      }
-    } catch (IOException ignore) {
+    private void loggingRequest(SpringRequestWrapper request) {
+        try {
+            if (includeHeaders) {
+                log()
+                    .info(
+                        "-->Request: method={}, uri={}, body={}, headers={}",
+                        request.getMethod(),
+                        request.getRequestURI(),
+                        request.getBodyString(),
+                        request.getAllHeaders());
+            } else {
+                log()
+                    .info(
+                        "-->Request: method={}, uri={}, body={}",
+                        request.getMethod(),
+                        request.getRequestURI(),
+                        request.getBodyString());
+            }
+        } catch (IOException ignore) {
+        }
     }
-  }
 
-  private void loggingResponse(long start, SpringRespondWrapper respond) {
-    loggingResponse(start, respond, respond.getStatus());
-  }
-
-  private void loggingResponse(long start, SpringRespondWrapper respond, int status) {
-    try {
-      final long cost = System.currentTimeMillis() - start;
-      respond.setCharacterEncoding("UTF-8");
-      if (includeHeaders) {
-        log()
-            .info(
-                "<--Respond: cost={}ms, status={}, body={}, headers={}",
-                cost,
-                status,
-                IOUtils.toString(respond.getContentAsByteArray(), respond.getCharacterEncoding()),
-                respond.getAllHeaders());
-      } else {
-        log()
-            .info(
-                "<--Respond: cost={}ms, status={}, body={}",
-                cost,
-                status,
-                IOUtils.toString(respond.getContentAsByteArray(), respond.getCharacterEncoding()));
-      }
-    } catch (IOException ignore) {
+    private void loggingResponse(long start, SpringRespondWrapper respond) {
+        loggingResponse(start, respond, respond.getStatus());
     }
-  }
 
-  private void fillHandlerMethod(HttpServletRequest request) {
-    try {
-      RequestMappingHandlerMapping mappings =
-          (RequestMappingHandlerMapping) context.getBean("requestMappingHandlerMapping");
-      HandlerExecutionChain handlerChain = mappings.getHandler(request);
-
-      if (handlerChain != null) {
-        HandlerMethod handler = (HandlerMethod) handlerChain.getHandler();
-        MDC.put(
-            Consts.OperaHeader,
-            handler.getBeanType().getSimpleName() + "." + handler.getMethod().getName());
-      }
-    } catch (Exception ex) {
-      log().trace("Cannot extract handler method", ex);
+    private void loggingResponse(long start, SpringRespondWrapper respond, int status) {
+        try {
+            final long cost = System.currentTimeMillis() - start;
+            respond.setCharacterEncoding("UTF-8");
+            if (includeHeaders) {
+                log()
+                    .info(
+                        "<--Respond: cost={}ms, status={}, body={}, headers={}",
+                        cost,
+                        status,
+                        respond.getBodyString(),
+                        respond.getAllHeaders());
+            } else {
+                log()
+                    .info(
+                        "<--Respond: cost={}ms, status={}, body={}",
+                        cost,
+                        status,
+                        respond.getBodyString());
+            }
+        } catch (IOException ignore) {
+        }
     }
-  }
+
+    private String formatCleanBody() {
+        return "";
+    }
+
+    private void fillHandlerMethod(HttpServletRequest request) {
+        try {
+            RequestMappingHandlerMapping mappings =
+                (RequestMappingHandlerMapping) context.getBean("requestMappingHandlerMapping");
+            HandlerExecutionChain handlerChain = mappings.getHandler(request);
+
+            if (handlerChain != null) {
+                HandlerMethod handler = (HandlerMethod) handlerChain.getHandler();
+                MDC.put(
+                    Consts.OperaHeader,
+                    handler.getBeanType().getSimpleName() + "." + handler.getMethod().getName());
+            }
+        } catch (Exception ex) {
+            log().trace("Cannot extract handler method", ex);
+        }
+    }
 }
